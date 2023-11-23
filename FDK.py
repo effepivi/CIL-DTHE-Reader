d773ab31c6931b1708b732a55ccf27bb58c89cbe
effepivi <franck.p.vidal@gmail.com>
2023-11-23 13:15:32 +0000

    Save the geometry into a file if needed.

--- FDK.py
+++ FDK.py
@@ -17,7 +17,9 @@ parser.add_argument("src",  type=str, help="Source location")
 parser.add_argument("dest", type=str, help="Destination location")
 parser.add_argument("-dg", "--display_geometry", action="store_true", help="Display the geometry using a Matplotlib figure")
 parser.add_argument("-pg", "--print_geometry", action="store_true", help="Print the geometry in the terminal")
+parser.add_argument("--save_geometry", type=str, help="Path of the file name where the plot of the geometry will be saved")
 args = parser.parse_args()
+
 
 # Create the reader
 reader = DTHEDataReader(args.src)
@@ -26,8 +28,12 @@ reader = DTHEDataReader(args.src)
 data = reader.read()
 
 # Display the geometry using a Matplotlib figure
-if args.display_geometry:
+if args.display_geometry and args.save_geometry is None:
     show_geometry(data.geometry)
+
+# Display the geometry using a Matplotlib figure and save it into a file
+if args.save_geometry is not None:
+    show_geometry(data.geometry).save(args.save_geometry)
 
 # Print the geometry in the terminal
 if args.print_geometry:
