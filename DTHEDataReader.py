706676e9d210a16298925544f8acb02473c249bc
effepivi <franck.p.vidal@gmail.com>
2023-11-23 12:49:04 +0000

    Working reader for DTHE.

--- DTHEDataReader.py
+++ DTHEDataReader.py
@@ -18,22 +18,14 @@
 # Franck P. Vidal (Science and Technology Facilities Council)
 
 
-from cil.framework import AcquisitionData, AcquisitionGeometry, ImageData, ImageGeometry, DataOrder
-# import numpy as np
+from cil.framework import AcquisitionGeometry #, AcquisitionData, ImageData, ImageGeometry, DataOrder
+from cil.io.TIFF import TIFFStackReader
+
+import numpy as np
 import os
 from pathlib import Path
 from xml.etree import ElementTree
 from tifffile import imread
-
-# import olefile
-# import logging
-# dxchange_logger = logging.getLogger('dxchange')
-# dxchange_logger.setLevel(logging.ERROR)
-
-# import dxchange
-# import warnings
-
-# logger = logging.getLogger(__name__)
 
 class DTHEDataReader(object):
 
@@ -44,31 +36,97 @@ class DTHEDataReader(object):
     ----------
     file_name: str
         file name to read
+
+    normalise: bool, default=True
+        normalises loaded projections by detector white level (I_0)
+
+    mode: str: {'bin', 'slice'}, default='bin'
+        In bin mode, 'step' number of pixels is binned together,
+        values of resulting binned pixels are calculated as average. 
+        In 'slice' mode 'step' defines standard numpy slicing.
+        Note: in general output array size in bin mode != output array size in slice mode
+
+    fliplr: bool, default = False,
+        flip projections in the left-right direction (about vertical axis)
+
+    Notes
+    -----
+    `roi` behaviour:
+        Files are stacked along axis_0. axis_1 and axis_2 correspond
+        to row and column dimensions, respectively.
+        
+        Files are stacked in alphabetic order. 
+        
+        To skip projections or to change number of projections to load, 
+        adjust 'angle'. For instance, 'angle': (100, 300)
+        will skip first 100 projections and will load 200 projections.
+        
+        ``'angle': -1`` is a shortcut to load all elements along axis.
+            
+        ``start`` and ``end`` can be specified as ``None`` which is equivalent
+        to ``start = 0`` and ``end = load everything to the end``, respectively.
+        Start and end also can be negative.
     '''
     
-    def __init__(self, file_name: str=None):
+    def __init__(self,
+                 file_name: str=None,
+                 normalise: bool=True,
+                 mode: str="bin",
+                 fliplr: bool=False):
 
         # Initialise class attributes to None
         self.file_name = None
-        self._geometry = None
+        self.normalise = normalise
+        self.mode = mode
+        self.fliplr = fliplr
+        self._ag = None # The acquisition geometry object
+        self.tiff_directory_path = None
 
-        # # Set logging level for dxchange reader.py
-        # logger_dxchange = logging.getLogger(name='dxchange.reader')
-        # if logger_dxchange is not None:
-        #     logger_dxchange.setLevel(logging.ERROR)
-
+        # The file name is set
         if file_name is not None:
-            self.set_up(file_name)
+
+            # Initialise the instance
+            self.set_up(file_name=file_name,
+                normalise=normalise,
+                fliplr=fliplr)
 
 
-    def set_up(self, file_name: str):
+    def set_up(self,
+               file_name: str=None,
+               normalise: bool=True,
+               mode: str="bin",
+               fliplr: bool=False):
+
         '''Set up the reader
         
         Parameters
         ----------
         file_name: str
             file name to read
+
+        normalise: bool, default=True
+            normalises loaded projections by detector white level (I_0)
+
+        mode: str: {'bin', 'slice'}, default='bin'
+            In bin mode, 'step' number of pixels is binned together,
+            values of resulting binned pixels are calculated as average. 
+            In 'slice' mode 'step' defines standard numpy slicing.
+            Note: in general output array size in bin mode != output array size in slice mode
+
+        fliplr: bool, default = False,
+            flip projections in the left-right direction (about vertical axis)
         '''
+
+        # Save the attributes
+        self.file_name = file_name
+        # self.roi = roi
+        self.normalise = normalise
+        self.mode = mode
+        self.fliplr = fliplr
+
+        # Check a file name was provided
+        if file_name is None:
+            raise ValueError('Path to unireconstruction.xml file is required.')
 
         # Check if the file exists
         file_name = os.path.abspath(file_name)
@@ -84,14 +142,11 @@ class DTHEDataReader(object):
         directory_path = Path(os.path.dirname(file_name))
 
         # Look for projections
-        projection_path = directory_path / "Proj"
-        if not os.path.isdir(projection_path):
-            raise ValueError(f"The projection directory '{projection_path}' does not exist")
+        self.tiff_directory_path = directory_path / "Proj"
+        if not os.path.isdir(self.tiff_directory_path):
+            raise ValueError(f"The projection directory '{self.tiff_directory_path}' does not exist")
 
-        # Save the file name
-        self.file_name = file_name
-
-        # Open the file
+        # Open the XML file
         tree = ElementTree.parse(file_name)
 
         # Find the conebeam profile
@@ -102,7 +157,7 @@ class DTHEDataReader(object):
         number_of_projections = int(profile.attrib["images"])
 
         # Look for the name of projection images
-        image_file_names = [image for image in projection_path.rglob("*.tif")]
+        image_file_names = [image for image in self.tiff_directory_path.rglob("*.tif")]
         assert (len(image_file_names) == number_of_projections)
 
         # Find the acquisition information
@@ -116,35 +171,84 @@ class DTHEDataReader(object):
         # Get the SDD and SOD
         source_to_detector = float(conf_geo.attrib["sdd"])
         source_to_object = float(conf_geo.attrib["sod"])
+        object_to_detector = source_to_detector - source_to_object
 
         # Known values from the manufacturer
         pixel_size_in_um = 150
         pixel_size_in_mm = pixel_size_in_um * 0.001
 
-
-
-
         # Create the acquisition geometry
-        self._geometry = AcquisitionGeometry.create_Cone3D(
-            source_position=[-source_to_object,0 , 0], 
+        self._ag = AcquisitionGeometry.create_Cone3D(
+            source_position=[-source_to_object, 0, 0], 
             detector_direction_x=[0, -1,  0],
             detector_direction_y=[0,  0, -1],
             detector_position=[object_to_detector, 0, 0], 
             rotation_axis_position=[0, 0, 0])
 
         # Set the angles of rotation
-        self._geometry.set_angles(np.linspace(0, 360, number_of_projections))
+        self._ag.set_angles(np.linspace(0, 360, number_of_projections))
 
-        self._geometry.set_panel(detector_number_of_pixels, pixel_spacing_mm)
-        self._geometry.set_labels(['angle','vertical','horizontal'])
+        # Read the first projection to extract its size in nmber of pixels
+        first_projection_data = imread(image_file_names[0])
+        projections_shape = (number_of_projections, *first_projection_data.shape)
+        
+        # self._ag.set_panel(detector_number_of_pixels, pixel_spacing_mm)
+        self._ag.set_labels(['angle','vertical','horizontal'])
 
-        # Panel is height x width
-        self._geometry.set_panel(projections_shape[1:][::-1], pixel_size_in_mm)
+        # Panel is width x height
+        self._ag.set_panel(first_projection_data.shape[::-1], pixel_size_in_mm)
 
+
+    def read(self):
+        
+        '''
+        Reads projections and returns AcquisitionData with corresponding geometry,
+        arranged as ['angle', horizontal'] if a single slice is loaded
+        and ['vertical, 'angle', horizontal'] if more than 1 slice is loaded.
+        '''
+
+        # Check a file name was provided
+        if self.tiff_directory_path is None:
+            raise ValueError('The reader was not set properly.')
+
+        # Create the TIFF reader
+        reader = TIFFStackReader()
+
+        reader.set_up(file_name=self.tiff_directory_path,
+                    #   roi=roi,
+                      mode=self.mode)
+
+        ad = reader.read_as_AcquisitionData(self._ag)
+              
+        if (self.normalise):
+            white_level = np.max(ad.array)
+            ad.array[ad.array < 1] = 1
+
+            # cast the data read to float32
+            ad = ad / np.float32(white_level)
+            
+        
+        if self.fliplr:
+            dim = ad.get_dimension_axis('horizontal')
+            ad.array = np.flip(ad.array, dim)
+        
+        return ad
+
+    def load_projections(self):
+        '''alias of read for backward compatibility'''
+        return self.read()
+
+
+    def get_geometry(self):
+        
+        '''
+        Return AcquisitionGeometry object
+        '''
+        
+        return self._ag
 
     def get_geometry(self):
         '''
         Return the acquisition geometry object
         '''
-        return self._geometry
-    
+        return self._ag
